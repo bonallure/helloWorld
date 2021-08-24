84122878ebb01cac5f8f8a5cd27e5635a93ddcda
lmwamba-hai <bmwamba@stedwards.edu>
2021-08-24 18:25:12 -0500

    initial commit

---
+++ src/main/java/com/company/Hello.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by bonallure on 8/24/21
+ */
+public class Hello {
+
+    public static void main (String[] args) {
+
+        System.out.println("Hello, World!");
+
+    }
+}
